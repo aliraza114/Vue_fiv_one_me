cc13aa2bc8a35e62e5bb2e1de2352003c9fee6c7
aliraza114 <aliraza3569@gmail.com>
2020-08-29 12:42:41 +0500

    project setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+// Vue imports
 import Vue from 'vue'
 import App from './App.vue'
+
+// Vue libs import
+import router from './router/router'
+import vuetify from 'vuetify/lib'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,22 @@
+// Vuejs imports
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// local imports
+import Register from '../components/Register'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/register',
+    name: 'Register',
+    component: Register
+}]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
